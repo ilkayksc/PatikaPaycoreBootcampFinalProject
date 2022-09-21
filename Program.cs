7adb12a3c568da1cdc9e206d15d7a3e84d7bdf27
İlkay Kesici <ilkayksc@gmail.com>
2022-09-22 00:21:57 +0300

    Project Completed

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatikaPaycoreBootcampFinalProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Read Configuration from appSettings
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+
+            // Initialize Logger
+            //Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+            Log.Information("Application is starting.");
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                //.UseSerilog() // add seri log 
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,89 @@
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PatikaPaycoreBootcampFinalProject.Model;
+using PatikaPaycoreBootcampFinalProject.StartUpExtension;
+using System.Net;
+
+namespace PatikaPaycoreBootcampFinalProject
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public static JwtConfig JwtConfig { get; private set; }
+       
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+            //hangfire 
+            services.AddHangfire(configuration => configuration
+               .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+               .UseSimpleAssemblyNameTypeSerializer()
+               .UseRecommendedSerializerSettings()
+               .UsePostgreSqlStorage(Configuration.GetConnectionString("PostgreSqlConnection"), new PostgreSqlStorageOptions
+               {
+                   TransactionSynchronisationTimeout = TimeSpan.FromMinutes(5),
+                   InvisibilityTimeout = TimeSpan.FromMinutes(5),
+                   QueuePollInterval = TimeSpan.FromMinutes(5),
+               }));
+            services.AddHangfireServer();
+            // hibernate
+            var connStr = Configuration.GetConnectionString("PostgreSqlConnection");
+            services.AddNHibernatePosgreSql(connStr);
+            services.AddHttpContextAccessor();
+
+            // Configure JWT Bearer
+            JwtConfig = Configuration.GetSection("JwtConfig").Get<JwtConfig>();
+            services.Configure<JwtConfig>(Configuration.GetSection("JwtConfig"));
+
+
+            // service
+            services.AddServices();
+            services.AddJwtBearerAuthentication();
+            services.AddSwaggerGen();
+            services.AddControllers();
+            services.AddCustomizeSwagger();
+            services.AddSingleton<IConfiguration>(Configuration);
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"));
+            }
+
+
+            
+            app.UseHttpsRedirection();
+
+
+            // add auth 
+            app.UseAuthentication();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseHangfireDashboard();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
