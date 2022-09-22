0329daddc4b9d7b53294075cff9ecb64eb3b6c99
İlkay Kesici <ilkayksc@gmail.com>
2022-09-22 10:22:31 +0300

    Update Color.cs

--- Model/Color.cs
+++ Model/Color.cs
@@ -2,9 +2,11 @@
 namespace PatikaPaycoreBootcampFinalProject.Model
 {
     public class Color
-    {
+    {   [Required]
         public virtual long Id { get; set; }
+        [Required]
         public virtual string ColorName { get; set; }
+        [Required]
         public virtual string ColorCode { get; set; }
         public Color()
         {
