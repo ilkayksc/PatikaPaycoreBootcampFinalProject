194d3ae563b2523e34cd02648f22fd43655f089f
İlkay Kesici <ilkayksc@gmail.com>
2022-09-22 10:23:11 +0300

    Update Category.cs

--- Model/Category.cs
+++ Model/Category.cs
@@ -2,8 +2,11 @@
 namespace PatikaPaycoreBootcampFinalProject.Model
 {
     public class Category
-    {   public virtual long Id { get; set; }
+    {   [Required]
+        public virtual long Id { get; set; }
+        [Required]
         public virtual string CategoryName { get; set; }
+        [Required]
         public virtual string CategoryDescription { get; set; }
         public Category()
         {
