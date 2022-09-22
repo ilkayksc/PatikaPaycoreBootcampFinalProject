f3b9461b80de6a4d0786f4c9017571138b4cce4a
İlkay Kesici <ilkayksc@gmail.com>
2022-09-22 10:22:07 +0300

    Update Buy.cs

--- Model/Buy.cs
+++ Model/Buy.cs
@@ -2,11 +2,13 @@
 namespace PatikaPaycoreBootcampFinalProject.Model
 {
     public class Buy
-    {
+    {   [Required]
         public virtual long Id { get; set; }
+        [Required]
         public virtual double Price { get; set; }
         public virtual long ProductOwner { get; set; }
         public virtual long Customer { get; set; }
+        [Required]
         public virtual long ProductId { get; set; }
         public Buy()
         {
