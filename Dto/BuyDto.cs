14cf0fb086cfc44d9c207122be1b71dd1938318e
İlkay Kesici <ilkayksc@gmail.com>
2022-09-22 10:20:39 +0300

    Update BuyDto.cs

--- Dto/BuyDto.cs
+++ Dto/BuyDto.cs
@@ -2,11 +2,16 @@
 namespace PatikaPaycoreBootcampFinalProject.Dto
 {
     public class BuyDto
-    {
+    {   [Required]
         public virtual long Id { get; set; }
+        [Required]
         public virtual double Price { get; set; }
+     
         public virtual long ProductOwner { get; set; }
+       
         public virtual long Customer { get; set; }
+     
+        [Required]
         public virtual long ProductId { get; set; }
         public BuyDto()
         {
