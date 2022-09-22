9d14091951b36216ece1cec6ef8588a866db810d
İlkay Kesici <ilkayksc@gmail.com>
2022-09-22 10:05:50 +0300

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PatikaPaycoreBootcampFinalProject.Base;
+using PatikaPaycoreBootcampFinalProject.Context;
+using PatikaPaycoreBootcampFinalProject.Dto;
+using PatikaPaycoreBootcampFinalProject.Model;
+using PatikaPaycoreBootcampFinalProject.Services;
+using PatikaPaycoreBootcampFinalProject.StartUpExtension;
+using ISession = NHibernate.ISession;
+
+namespace PatikaPaycoreBootcampFinalProject.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ISession session;
+        private readonly ICategoryService _category;
+
+        public CategoryController(ISession session, ICategoryService _category)
+        {
+          
+            this._category = _category;
+            this.session = session;
+          
+        }
+
+        [HttpGet]
+        public BaseResponse<IEnumerable<CategoryDto>> Get()
+        {
+            var categories = _category.GetAll();
+            return categories;
+        }
+
+        
+        [HttpGet("GetCategoryById")]
+        public BaseResponse<CategoryDto> GetCategoryById([FromQuery] long id)
+        {   
+            var category = _category.GetById(id);
+            return category;
+        }
+
+        [HttpPost]
+        public BaseResponse<CategoryDto> CreateCategory([FromBody] CategoryDto request)
+        {
+            var response = _category.Insert(request);
+            return response;
+        }
+
+        [HttpPut("/UpdateCategory/{id}")]
+        public BaseResponse<CategoryDto> UpdateCategory(long id, [FromBody] CategoryDto request)
+        {   
+            var result = _category.Update(id,request);
+            return result;
+        }
+        [HttpDelete("/DeleteCategory/{id}")]
+        public ActionResult DeleteCategory(long id)
+        {
+            
+            _category.Remove(id);
+            
+            return Ok();
+        }
+    }
+}
+
---
+++ Controllers/ColorController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PatikaPaycoreBootcampFinalProject.Base;
+using PatikaPaycoreBootcampFinalProject.Context;
+using PatikaPaycoreBootcampFinalProject.Dto;
+using PatikaPaycoreBootcampFinalProject.Model;
+using PatikaPaycoreBootcampFinalProject.Services;
+using PatikaPaycoreBootcampFinalProject.StartUpExtension;
+using ISession = NHibernate.ISession;
+
+namespace PatikaPaycoreBootcampFinalProject.Controllers
+{
+  
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorController : ControllerBase
+    {
+        private readonly ISession session;
+        private readonly IColorService _color;
+
+        public ColorController(ISession session, IColorService _color)
+        {
+
+            this._color = _color;
+            this.session = session;
+
+        }
+
+        [HttpGet]
+        public BaseResponse<IEnumerable<ColorDto>> Get()
+        {
+            var color = _color.GetAll();
+            return color;
+        }
+
+
+        [HttpGet("GetColorById")]
+        public BaseResponse<ColorDto> GetColorById([FromQuery] long id)
+        {
+            var color = _color.GetById(id);
+            return color;
+        }
+
+        [HttpPost]
+        public BaseResponse<ColorDto> CreateColor([FromBody] ColorDto request)
+        {
+            var response = _color.Insert(request);
+            return response;
+        }
+
+        [HttpPut("/UpdateColor/{id}")]
+        public BaseResponse<ColorDto> UpdateColor(long id, [FromBody] ColorDto request)
+        {
+            var result = _color.Update(id, request);
+            return result;
+        }
+        [HttpDelete("/DeleteColor/{id}")]
+        public ActionResult DeleteColor(long id)
+        {
+
+            _color.Remove(id);
+
+            return Ok();
+        }
+    }
+}
+
