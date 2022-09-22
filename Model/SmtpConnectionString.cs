5ab733a681805c20da2089740139a21d2fcc5b8d
İlkay Kesici <ilkayksc@gmail.com>
2022-09-22 09:13:49 +0300

    SmtpConnectionString Class Added.

---
+++ Model/SmtpConnectionString.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PatikaPaycoreBootcampFinalProject.Model
+{
+    public class SmtpConnectionString
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public SmtpConnectionString()
+        {
+        }
+    }
+}
+
