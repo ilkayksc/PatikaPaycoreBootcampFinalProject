7bddd2c39e929100cc0286aa708d208a7d7d5f96
İlkay Kesici <ilkayksc@gmail.com>
2022-09-22 10:07:43 +0300

    Add files via upload

---
+++ Context/IMapperSession.cs
@@ -0,0 +1,27 @@
+﻿using PatikaPaycoreBootcampFinalProject.Model;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PatikaPaycoreBootcampFinalProject.Context
+{
+    public interface IMapperSession<Entity> where Entity:class
+    {
+        void BeginTransaction();
+        void Commit();
+        void Rollback();
+        void CloseTransaction();
+
+        void Save(Entity entity);
+        void Update(Entity entity);
+        void Delete(long id);
+        List<Entity> GetAll();
+
+        IQueryable<Entity> Entities { get; }
+        IEnumerable<Entity> Where(Expression<Func<Entity, bool>> where);
+        Entity GetById(long id);
+
+
+
+    }
+}
