df16769b94b883e8709f821c164c7b79a287215b
İlkay Kesici <ilkayksc@gmail.com>
2022-09-22 10:21:01 +0300

    Update CategoryDto.cs

--- Dto/CategoryDto.cs
+++ Dto/CategoryDto.cs
@@ -2,9 +2,11 @@
 namespace PatikaPaycoreBootcampFinalProject.Dto
 {
     public class CategoryDto
-    {
+    {   [Required]
         public virtual long Id { get; set; }
+        [Required]
         public virtual string CategoryName { get; set; }
+        [Required]
         public virtual string CategoryDescription { get; set; }
         
         public CategoryDto()
